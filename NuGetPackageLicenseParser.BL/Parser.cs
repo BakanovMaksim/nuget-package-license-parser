66d5afd3d2236391d04c758d90d07f593232fbbf
maksim <bakanovmp12@gmail.com>
2020-07-30 14:56:16 +0300

    feature: parsing licenses nuget packages

--- NuGetPackageLicenseParser.BL/Parser.cs
+++ NuGetPackageLicenseParser.BL/Parser.cs
@@ -1,7 +1,11 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NuGetPackageLicenseParser.BL
 {
@@ -14,29 +18,67 @@ namespace NuGetPackageLicenseParser.BL
             var allFilesProjects = directoryProject.GetFiles("project.nuget.cache", SearchOption.AllDirectories).Where(p => !p.DirectoryName.Contains("NuGetPackageLicenseParser"));
 
             var linksNuGet = new List<string>();
+            var linksLicense = new List<string>();
+            var fileName = new List<string>();
 
             foreach (var item in allFilesProjects)
-            {
-                var file = File.ReadAllLines(item.FullName);
-                var lin = file.Where(p => p.Contains(".nuget")).Select(p => p); 
-                linksNuGet.AddRange(lin); 
-            }
+                linksNuGet.AddRange(ParseToFiles(item, ".nuget"));
 
             DirectoryInfo directoryPackage = null;
 
-            foreach(var item in linksNuGet)
+            foreach (var item in linksNuGet)
             {
                 var text = item.Remove(item.Length - 2).Remove(0, 5);
                 var directoryfdh = new DirectoryInfo(Path.GetFullPath(text));
-                var file = new FileInfo(directoryfdh.FullName); 
+                var filefdh = new FileInfo(directoryfdh.FullName);
 
-                directoryPackage = new DirectoryInfo(file.DirectoryName);
+                directoryPackage = new DirectoryInfo(filefdh.DirectoryName);
 
-                foreach(var itemFile in directoryPackage.GetFiles())
+                ForeachDirectory(directoryPackage, linksLicense, fileName);
+            }
+
+            for (int k = 0; k < linksLicense.Count; k++)
+                File.WriteAllText($"{ConfigurationManager.AppSettings["path"]}{fileName[k].Replace("<id>", "").Replace("</id>", "")}",
+                    LoadPage($"{linksLicense[k].Replace("<licenseUrl>", "").Replace("</licenseUrl>", "")}"));
+        }
+
+        private static IEnumerable<string> ParseToFiles(FileInfo item, string text) => File.ReadAllLines(item.FullName).Where(p => p.Contains(text));
+
+        private static void ForeachDirectory(DirectoryInfo directoryPackage, List<string> linksLicense, List<string> fileName)
+        {
+            foreach (var itemFile in directoryPackage.GetFiles())
+            {
+                if (itemFile.Name.Contains("LICENSE")) File.Copy(itemFile.FullName, Path.GetFullPath($"{ConfigurationManager.AppSettings["path"]}" + Directory.GetParent(itemFile.DirectoryName).Name), true);
+                else if (itemFile.Name.Contains(".nuspec"))
                 {
-                    if (itemFile.Name.Contains(".nuspec")) Console.WriteLine(itemFile.Name);
+                    linksLicense.AddRange(ParseToFiles(itemFile, "licenseUrl"));
+                    fileName.AddRange(ParseToFiles(itemFile, "<id>"));
                 }
             }
+        }
+
+        private static string LoadPage(string url)
+        {
+            var result = "";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                if (receiveStream != null)
+                {
+                    StreamReader readStream;
+                    if (response.CharacterSet == null)
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    result = readStream.ReadToEnd();
+                    readStream.Close();
+                }
+                response.Close();
+            }
+            return Regex.Replace(result, "<[^>]+>|&quot;", string.Empty);
         }
     }
 }
